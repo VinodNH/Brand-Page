8f4cd2c6c08e87b58d435358e466e3fd8c24c251
VinodNH <vinnunh@gmail.com>
2024-09-23 23:36:10 +0530

    Initial commit

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,18 @@
+const Navigation = () => {
+  return (
+    <nav>
+      <img src="public/images/nike.png" />
+
+      <ul>
+        <li href="#">MENU</li>
+        <li href="#">LOCATION</li>
+        <li href="#">ABOUT</li>
+        <li href="#">CONTACT</li>
+      </ul>
+
+      <button>Login</button>
+    </nav>
+  );
+};
+
+export default Navigation;
